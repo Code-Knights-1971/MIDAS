74da1a885c4cee6e319d6c36753be41e53d7154d
Abdul razak <abdulrazak.nasriudeen@gmail.com>
2025-04-12 12:14:33 +0530

    Created models

---
+++ backend/xyntra-midas-springboot/src/main/java/com/hackathon/xyntra_midas_springboot/model/MissileData.java
@@ -0,0 +1,23 @@
+package com.hackathon.xyntra_midas_springboot.model;
+
+// MissileData.java
+
+public class MissileData {
+    public String missileId;
+    public String launchTime;
+    public Position currentPosition;
+    public double speed_mps;
+    public double direction_deg;
+    public double altitude_m;
+
+    // Getters and Setters
+
+
+}
+  class Position {
+      public double latitude;
+      public double longitude;
+
+    // Getters and Setters
+}
+
---
+++ backend/xyntra-midas-springboot/src/main/java/com/hackathon/xyntra_midas_springboot/model/User.java
@@ -0,0 +1,53 @@
+package com.hackathon.xyntra_midas_springboot.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+// Getters and Setters
+}
+
