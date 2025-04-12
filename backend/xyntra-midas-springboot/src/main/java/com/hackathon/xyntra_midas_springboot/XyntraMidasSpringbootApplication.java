1aecfddda183879c85fa38e38dcab91e9702106b
Abdul razak <abdulrazak.nasriudeen@gmail.com>
2025-04-12 10:51:20 +0530

    Spring boot setup

---
+++ backend/xyntra-midas-springboot/src/main/java/com/hackathon/xyntra_midas_springboot/XyntraMidasSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.xyntra_midas_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyntraMidasSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyntraMidasSpringbootApplication.class, args);
+	}
+
+}
---
+++ backend/xyntra-midas-springboot/src/test/java/com/hackathon/xyntra_midas_springboot/XyntraMidasSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.xyntra_midas_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyntraMidasSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
